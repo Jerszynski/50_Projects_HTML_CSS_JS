de74ecbb6508ad165cee13eef1a12878083e77cb
Jerszynski <krzysztof.jerszynski@o2.pl>
2023-10-04 09:44:43 +0200

    Add function to add and remove highlight class

--- random_choise_picker/script.js
+++ random_choise_picker/script.js
@@ -36,10 +36,24 @@ function randomSelect() {
 
   const interval = setInterval(() => {
     const randomTag = pickRandomTag();
+
+    highlightTag(randomTag);
+
+    setTimeout(() => {
+      unHighlightTag(randomTag);
+    }, 100);
   }, 100);
 }
 
 function pickRandomTag() {
-  const tags = document.querySelectorAll(".tags");
+  const tags = document.querySelectorAll(".tag");
   return tags[Math.floor(Math.random() * tags.length)];
+}
+
+function highlightTag(tag) {
+  tag.classList.add("highlight");
+}
+
+function unHighlightTag(tag) {
+  tag.classList.remove("highlight");
 }
