a7fbcdc9bffad6e55e28bfae4f3b9d2b215e569d
Jerszynski <krzysztof.jerszynski@o2.pl>
2025-01-19 16:59:30 +0100

    Add small fix to html, css, js files

--- movie_app/script.js
+++ movie_app/script.js
@@ -33,7 +33,9 @@ function showMovies(movies) {
         <img src="${IMG_PATH + poster_path}" alt="${title}" />
         <div class="movie-info">
           <h3>${title}</h3>
-          <span class="${getClassByRate(vote_average)}">${vote_average}</span>
+          <span class="${getClassByRate(vote_average)}">${vote_average.toFixed(
+      1
+    )}</span>
         </div>
         <div class="overview">
           <h3>Overview</h3>
